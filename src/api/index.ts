aea6ccde6a747d826ce39a5599792038f801db2d
Abdullah Alaftan <as.alaftan@gmail.com>
2024-05-28 12:17:21 +0300

    changed url

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ let baseURL = 'http://localhost:5125/api/v1'
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = 'http://localhost:5125/api/v1'
+  baseURL = 'https://backend-project-yazo.onrender.com/api/v1'
 }
 
 const api = axios.create({
